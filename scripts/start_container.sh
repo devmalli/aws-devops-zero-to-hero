477aca03ec4e3a0b264c664a37237bb7f47ea435
devmalli <149128498+devmalli@users.noreply.github.com>
2023-11-23 22:48:12 -0800

    Update start_container.sh

--- scripts/start_container.sh
+++ scripts/start_container.sh
@@ -5,4 +5,4 @@ set -e
 docker pull malli786/simple-python-app
 
 # Run the Docker image as a container
-docker run -d -p 5000:5000 malli786/simple-python-app
+docker run -d -p 5000:5000 malli786/simple-python-app 
